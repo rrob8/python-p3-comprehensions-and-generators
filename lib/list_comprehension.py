21b5649228e0c09a24e86ed1a99adfc1bd60dbc5
Robert Bubnik <rbubnik@gmail.com>
2023-12-24 12:15:16 -0500

    passing all tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if (n/2).is_integer() == True ]
+    return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed = [n + '!' for n in sentence_list]
+    return exclaimed
